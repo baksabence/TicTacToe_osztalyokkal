a7a4d8887bc6a75905d79d40a32cf2b54fbf9954
Baksa Bence <baksa.bence@dk.akkszalezi.hu>
2024-09-17 15:45:31 +0200

    mvc

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
+import TTTModell from "./modell/TTTModel.js"
 import Palya from "./view/Palya.js"
 
 const taroloELEM = $(".jatekter")
-const LISTA = ["","","","","","","","",""]
-let lepes = 0;
 
-new Palya(LISTA, taroloELEM)
+const tttModell = new TTTModell()
+
+new Palya(tttModell.getList(), taroloELEM)
 
 $(window).on("kattintas", (event)=>{
     console.log(event.detail)
-    if (lepes % 2 == 0) {
-        LISTA[event.detail]="X"    
-    }else{
-        LISTA[event.detail]="O"
-    }
-    lepes++;
-    console.log(LISTA)
-    new Palya(LISTA, taroloELEM)
+    tttModell.lepteto(event.detail)
+    new Palya(tttModell.getList(), taroloELEM)
 })
